160f38e2306ce22627b90ed3c7cf7044cca43155
BIT student <classroom@bgit.rs>
2018-02-05 15:05:45 +0100

    Dodavanje

---
+++ if_else/if_else.js
@@ -0,0 +1,12 @@
+var x=100
+var y=50
+var z=30
+
+if (x<y && x<z) {
+    console.log(1 + y +z);
+   
+}else if (y < x && y < z) {
+    console.log(x + 1 +z);
+}else {
+    console.log(x + y +1);
+}
---
+++ if_else/maximum.js
@@ -0,0 +1,4 @@
+var x=33;
+var y=9;
+
+x > y ? console.log(x) : console.log(y);
