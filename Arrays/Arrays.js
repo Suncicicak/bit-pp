17a1d2c38f88a8df7615f14b6fef023deda703bb
BIT student <classroom@bgit.rs>
2018-02-05 17:59:17 +0100

    Nizovi pocetak

---
+++ Arrays/Arrays.js
@@ -0,0 +1,11 @@
+// var a=["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+// console.log(a[5],a[9],a[0]);
+
+// var x=["Monday","Tuesday","Wednesday","Thursday","Friday", "Saturday", "Sunday"];
+// console.log(x[6]);
+
+// var x=[2, -4, 5, -2, -11];
+
+// console.log(x[1],x[3],x[4]);
+
+ 
