305ce84839afeb33dc35621e1f2d24847114126b
BIT student <classroom@bgit.rs>
2018-02-05 16:41:54 +0100

    Switch zadaci

---
+++ switch/Exercises.js
@@ -0,0 +1,129 @@
+// var day = 13;
+
+// switch (day) {
+//     case 1:
+//     console.log('Weekday');
+//     break;
+//     case 2:
+//     console.log('Tuesday');
+//     break;
+//     case 3:
+//     console.log('Wednesday');
+//     break;
+//     case 4:
+//     console.log('Thursday');
+//     break;
+//     case 5:
+//     console.log('Friday');
+//     break;
+//     case 6:
+//     console.log('Saturday');
+//     break;
+//     case 7:
+//     console.log('Sunday');
+//     break;
+//     default: 
+//     console.log("Input must be a number between 1 and 7")
+//     break;
+// }
+
+// var day = 66;
+
+// switch (day) {
+//     case 1: 
+  
+//     case 2:
+    
+//     case 3:
+   
+//     case 4:
+    
+//     case 5:
+//     console.log("It's weekday");
+//     break;
+//     case 6:
+   
+//     case 7:
+//     console.log("It's weekend");
+//     break;
+//     default: 
+//     console.log("Input must be a number between 1 and 7 ")
+//     break;
+// }
+
+var month = 8;
+
+switch (month) {
+    case 1:
+    console.log('January');
+    break;
+    case 2:
+    console.log('February');
+    break;
+    case 3:
+    console.log('March');
+    break;
+    case 4:
+    console.log('April');
+    break;
+    case 5:
+    console.log('May');
+    break;
+    case 6:
+    console.log('June');
+    break;
+    case 7:
+    console.log('July');
+    break;
+    case 8:
+    console.log('August');
+    break;
+    case 9:
+    console.log('September');
+    break;
+     case 10:
+    console.log('October');
+    break;
+    case 11:
+    console.log('November');
+    break;
+    case 12:
+    console.log('December');
+    break;
+    case 6:
+    console.log('Saturday');
+    break;
+    
+    default: 
+    console.log("Input must be a number between 1 and 12")
+    break;
+}
+
+var month=35;
+
+switch (month) {
+    case 12:
+    case 1:
+    case 2:
+    console.log('Winter');
+   break;
+    case 3:
+    case 4:
+    case 5:
+    console.log('Spring');
+    break;
+    case 6:
+    case 7:
+    case 8:
+    console.log('Summer');
+    break;
+    case 9:
+    case 10:
+    case 11:
+    console.log('Autumn');
+    break;
+    default: 
+    console.log("Input must be a number between 1 and 12")
+    break;
+} 
+
