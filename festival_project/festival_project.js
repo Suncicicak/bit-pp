f5f4c028a0d3f6eab123711724a2608c61ed299a
asus k52j <suncicatomic@yahoo.com>
2018-02-25 18:26:10 +0100

    finished festival

--- festival_project/festival_project.js
+++ festival_project/festival_project.js
@@ -1,37 +1,38 @@
 'use strict';
 
-(function(){
-    
-    function Genre (name) {
+(function () {
+
+    function Genre(name) {
         this.name = name;
-        this.getData = function() {
+        this.getData = function () {
             var formattedString;
-            formattedString = this.name[0] + this.name[this.name.length - 1]; 
-            
+            formattedString = this.name[0] + this.name[this.name.length - 1];
+
             return formattedString.toUpperCase();
         }
-    } 
+    }
 
-    function Movie (nameOfMovie, genre, length) {
+    function Movie(nameOfMovie, genre, length) {
         this.title = nameOfMovie;
         this.genre = genre;
         this.length = length;
-        this.getData = function() {
+        this.getData = function () {
             return '' + nameOfMovie + ', ' + length + ', ' + genre.getData() + '';
         }
     }
 
-    function Program (date, movies, totalNumOfMovies) {
+    function Program(date, movies, totalNumOfMovies) {
         this.date = new Date(date);
         this.list = movies;
-        this.totalNumOfMovies = 0; 
+        this.totalNumOfMovies = 0;
         this.addMovie = function (titleOfTheMovie) {
             this.list.push(titleOfTheMovie);
+            this.totalNumberOfMovies = this.list.length;
         }
-        this.getData = function() {
+        this.getData = function () {
             var moviesLength = 0,
                 moviesList = '';
-            
+
             for (var i in movies) {
                 moviesLength += movies[i].length;
                 moviesList += movies[i].getData() + ', \n';
@@ -41,20 +42,48 @@
         }
     }
 
-    function Festival (name,list,numbers) {
+    function Festival(name, list, numbers) {
         this.name = name;
         this.listOfPrograms = [];
-        this.numOfAllMovies = numOfAllMovies;
+        this.numOfAllMovies = function () {
+            var counter = 0;
+            for (var i = 0; i < this.listOfPrograms.length; i++) {
+                console.log(this.listOfPrograms);
+                counter = counter + this.listOfPrograms[i].totalNumOfMovies;
+            }
+            return counter; ;
         this.addProgram = function (program) {
             this.listOfPrograms.push(program);
         }
-
+        this.getData = function () {}
+    }
     }
 
-    var drama = new Genre('Drama');
-    var movieOne = new Movie('12 Years a Slave', drama, '120');
-    var program = new Program('10.10.2020.', [movieOne], 1);
-    var p = new Program('10.10.2020.', [movieOne], );
+    function createMovie(title, length, genre) {
+        return new Movie(title, genre, length);
+    }
 
-    console.log(p.getData());
+    function createProgram(date) {
+        return new Program(date);
+    }
+
+    var fantasy = new Genre("fantasy");
+
+    var JusticeLeague = new Movie("Justice League", fantasy, 120);
+    var Deadpool = new Movie("Deadpool", fantasy, 110);
+    var program = new Program();
+    program.addMovie(JusticeLeague);
+    program.addMovie(Deadpool);
+
+
+    var program2 = new Program();
+    program2.addMovie(JusticeLeague);
+    program2.addMovie(Deadpool);
+    var sarajevskiFestival = new Festival("Sarajevski Festival");
+    sarajevskiFestival.addProgram(program);
+    sarajevskiFestival.addProgram(program2);
+    var program3 = createProgram("19 February 2019");
+
+    console.log(program3.getData());
+    
 })();
